625fedb7a1b4f59de540028cc1bf457df8672f67
AlexeyTen <aten9180929937@gmail.com>
2024-03-09 12:13:18 +0300

    first commit

---
+++ ToDoListPresenter.java
@@ -0,0 +1,25 @@
+public class ToDoListPresenter {
+    private ToDoListView view;
+    private ToDoListModel model;
+    private boolean flag;
+
+    public ToDoListPresenter(ToDoListView view, ToDoListModel model) {
+        this.model = model;
+        this.view = view;
+    }
+
+    public void menu(int choise) {
+        while (flag) {
+            switch (choise) {
+                case 1: addTask(null);
+                case 2: showToDoList();
+                case 3: 
+
+
+                default:
+                    break;
+            }
+        }
+    }
+    
+}
---
+++ ToDoListView.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ToDoListView {
+    private Scanner scanner;
+
+    public ToDoListView() {
+        scanner = new Scanner(System.in);
+    }
+
+    public int choise(String choise) {
+        System.out.println(choise);
+        return scanner.nextInt();
+    }
+
+    public void showToDoList(ArrayList<String> toDoList) {
+        for (String task : toDoList) {
+            System.out.println(task);
+        }
+    }
+    
+
+}
